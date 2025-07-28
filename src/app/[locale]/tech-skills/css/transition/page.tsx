b268f9160066513a0dacf14b1832d946a95cd735
Valeriy Svystun <valeriy.all@gmail.com>
2025-07-28 21:41:35 +0300

    add: transition exercise

--- src/app/[locale]/tech-skills/css/transition/page.tsx
+++ src/app/[locale]/tech-skills/css/transition/page.tsx
@@ -1,8 +1,27 @@
+import css from "./page.module.css";
+
 export default function Transiton() {
   return (
     <div>
-      <h2>Transition</h2>
-      <p>Приклад Transition</p>
+      <h2 className={css.header}>Transition</h2>
+
+      <ul className={css.underlineMenu}>
+        <li>
+          <a href="" className={css.underlineMenuLink}>
+            Link 1
+          </a>
+        </li>
+        <li>
+          <a href="" className={css.underlineMenuLink}>
+            Link 2
+          </a>
+        </li>
+        <li>
+          <a href="" className={css.underlineMenuLink}>
+            Link 3
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
